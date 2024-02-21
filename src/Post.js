890c8f3c3d4bcaf98f50a4f2ceb436f75f8ade39
Ted Lee <tedlee000@hotmail.com>
2024-02-21 16:19:55 -0500

    first commit

---
+++ src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return (
+        <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2024/01/pixta_94590765_L.jpg?w=1390&crop=1" alt=""></img>
+      </div>
+        <div className="texts">
+          <h2>Dili wants to automate due diligence with AI</h2>
+          <p className="info">
+            <span className="author">Ted Lee</span>
+            <time>2024-02-19 17:12</time>
+          </p>
+          <p className="summary">Stephanie Song, formerly on the corporate development and ventures team at Coinbase, was often frustrated by the volume of due diligence tasks she and her team had to complete on a daily basis</p>
+        </div>
+    </div>
+    );
+}
